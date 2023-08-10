57da97a7d08186d75e55e61c0ea0da0aad140a24
Iamneo <techadmin@iamneo.ai>
2023-08-10 06:02:21 +0000

    Backup Commit

--- Project/src/test/java/testcases/Testcase1.java
+++ Project/src/test/java/testcases/Testcase1.java
@@ -7,7 +7,7 @@ import java.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.log4j.Logger;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
